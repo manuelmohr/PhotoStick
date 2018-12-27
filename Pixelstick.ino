ae8b47f9d4892acf53cdcad828a1b3b03c1586db
Manuel Mohr <manuelmohr@gmail.com>
2018-12-27 19:34:11 +0100

    Clean up and use clang-format.

--- Pixelstick.ino
+++ Pixelstick.ino
@@ -28,24 +28,41 @@ struct PixelFile
 
 PixelFile *pixelFile = nullptr;
 
-#define GUI_MAX_FONTS           2
-#define GUI_MAX_PAGES           1
-#define GUI_MAX_ELEMS_RAM       1
+#define GUI_MAX_FONTS 2
+#define GUI_MAX_PAGES 1
+#define GUI_MAX_ELEMS_RAM 1
 #define GUI_MAX_ELEMS_PER_PAGE 17
 
-gslc_tsGui     *guiGui      = nullptr;
-gslc_tsDriver  *guiDriver   = nullptr;
-gslc_tsPage    *guiPages    = nullptr;
-gslc_tsElem    *guiElem     = nullptr;
+gslc_tsGui *    guiGui      = nullptr;
+gslc_tsDriver * guiDriver   = nullptr;
+gslc_tsPage *   guiPages    = nullptr;
+gslc_tsElem *   guiElem     = nullptr;
 gslc_tsElemRef *guiElemRefs = nullptr;
 // Must be link-time constant as it is referenced by gslc_ElemCreateTxt_P
 // and other macros, to be put into PROGMEM.
-gslc_tsFont     guiFonts[GUI_MAX_FONTS];
+gslc_tsFont guiFonts[GUI_MAX_FONTS];
 
-enum {E_PG_MAIN};
-enum {E_ELEM_BOX,E_ELEM_BTN_QUIT,E_ELEM_COLOR,
-      E_SLIDER_R,E_SLIDER_G,E_SLIDER_B,E_ELEM_BTN_ROOM};
-enum {E_FONT_TXT,E_FONT_TITLE};
+enum
+{
+  E_PG_MAIN,
+};
+
+enum
+{
+  E_ELEM_BOX,
+  E_ELEM_BTN_QUIT,
+  E_ELEM_COLOR,
+  E_SLIDER_R,
+  E_SLIDER_G,
+  E_SLIDER_B,
+  E_ELEM_BTN_ROOM
+};
+
+enum
+{
+  E_FONT_TXT,
+  E_FONT_TITLE
+};
 
 static int16_t glscDebugOut(char ch)
 {
@@ -53,7 +70,8 @@ static int16_t glscDebugOut(char ch)
   return 0;
 }
 
-bool CbBtnQuit(void* pvGui,void *pvElemRef,gslc_teTouch eTouch,int16_t nX,int16_t nY)
+bool CbBtnQuit(void *pvGui, void *pvElemRef, gslc_teTouch eTouch, int16_t nX,
+               int16_t nY)
 {
   return true;
 }
@@ -69,7 +87,8 @@ void initScreen()
   guiElem     = new gslc_tsElem();
   guiElemRefs = new gslc_tsElemRef[GUI_MAX_ELEMS_PER_PAGE];
 
-  if (!gslc_Init(guiGui, guiDriver, guiPages, GUI_MAX_PAGES, guiFonts, GUI_MAX_FONTS)) {
+  if (!gslc_Init(guiGui, guiDriver, guiPages, GUI_MAX_PAGES, guiFonts,
+                 GUI_MAX_FONTS)) {
     panic(F("failed1"));
   }
 
@@ -80,40 +99,30 @@ void initScreen()
     panic(F("failed3"));
   }
 
-  gslc_PageAdd(guiGui, E_PG_MAIN, guiElem, GUI_MAX_ELEMS_RAM, guiElemRefs, GUI_MAX_ELEMS_PER_PAGE);
+  gslc_PageAdd(guiGui, E_PG_MAIN, guiElem, GUI_MAX_ELEMS_RAM, guiElemRefs,
+               GUI_MAX_ELEMS_PER_PAGE);
 
   // Background flat color
-  gslc_SetBkgndColor(guiGui,GSLC_COL_GRAY_DK2);
+  gslc_SetBkgndColor(guiGui, GSLC_COL_GRAY_DK2);
 
-  // Create Title with offset shadow
-  #define TMP_COL1 (gslc_tsColor){ 32, 32, 60}
-  #define TMP_COL2 (gslc_tsColor){128,128,240}
+// Create Title with offset shadow
+#define TMP_COL1 \
+  (gslc_tsColor) \
+  {              \
+    32, 32, 60   \
+  }
+#define TMP_COL2 (gslc_tsColor){ 128, 128, 240 }
   // Note: must use title Font ID
-  gslc_ElemCreateTxt_P(guiGui,98,E_PG_MAIN,2,2,320,50,"Pixelstick",&guiFonts[1],
-          TMP_COL1,GSLC_COL_BLACK,GSLC_COL_BLACK,GSLC_ALIGN_MID_MID,false,false);
-  gslc_ElemCreateTxt_P(guiGui,99,E_PG_MAIN,0,0,320,50,"Pixelstick",&guiFonts[1],
-          TMP_COL2,GSLC_COL_BLACK,GSLC_COL_BLACK,GSLC_ALIGN_MID_MID,false,false);
+  gslc_ElemCreateTxt_P(guiGui, 98, E_PG_MAIN, 2, 2, 320, 50, "Pixelstick",
+                       &guiFonts[1], TMP_COL1, GSLC_COL_BLACK, GSLC_COL_BLACK,
+                       GSLC_ALIGN_MID_MID, false, false);
+  gslc_ElemCreateTxt_P(guiGui, 99, E_PG_MAIN, 0, 0, 320, 50, "Pixelstick",
+                       &guiFonts[1], TMP_COL2, GSLC_COL_BLACK, GSLC_COL_BLACK,
+                       GSLC_ALIGN_MID_MID, false, false);
 
   // Create background box
-  gslc_ElemCreateBox_P(guiGui,200,E_PG_MAIN,10,50,300,180,GSLC_COL_WHITE,GSLC_COL_BLACK,true,true,NULL,NULL);
-
-  // Create dividers
-  gslc_ElemCreateBox_P(guiGui,201,E_PG_MAIN,20,100,280,1,GSLC_COL_GRAY_DK3,GSLC_COL_BLACK,true,true,NULL,NULL);
-  gslc_ElemCreateBox_P(guiGui,202,E_PG_MAIN,235,60,1,35,GSLC_COL_GRAY_DK3,GSLC_COL_BLACK,true,true,NULL,NULL);
-
-  // Create dummy selector
-  gslc_ElemCreateTxt_P(guiGui,100,E_PG_MAIN,20,65,100,20,"Selected Room:",&guiFonts[0],
-          GSLC_COL_GRAY_LT2,GSLC_COL_BLACK,GSLC_COL_BLACK,GSLC_ALIGN_MID_LEFT,false,true);
-
-  gslc_ElemCreateTxt_P(guiGui,105,E_PG_MAIN,160,115,120,20,"Set LED RGB:",&guiFonts[0],
-          GSLC_COL_WHITE,GSLC_COL_BLACK,GSLC_COL_BLACK,GSLC_ALIGN_MID_LEFT,false,true);
-
-  // Create three sliders (R,G,B) and assign callback function
-  // that is invoked upon change. The common callback will update
-  // the color box.
-
-  gslc_ElemCreateTxt_P(guiGui,109,E_PG_MAIN,250,230,60,10,"GUIslice Example",&guiFonts[0],
-          GSLC_COL_BLACK,GSLC_COL_BLACK,GSLC_COL_BLACK,GSLC_ALIGN_MID_RIGHT,false,false);
+  gslc_ElemCreateBox_P(guiGui, 200, E_PG_MAIN, 10, 50, 300, 180, GSLC_COL_WHITE,
+                       GSLC_COL_BLACK, true, true, NULL, NULL);
 
   gslc_SetPageCur(guiGui, E_PG_MAIN);
   Serial.println(F("successful"));
@@ -121,7 +130,7 @@ void initScreen()
 
 void deinitScreen()
 {
-  gslc_SetBkgndColor(guiGui,GSLC_COL_BLACK);
+  gslc_SetBkgndColor(guiGui, GSLC_COL_BLACK);
   gslc_Update(guiGui);
 
   delete[] guiElemRefs;
@@ -140,10 +149,6 @@ void initSdCard()
   Serial.println(F("OK!"));
 }
 
-void deinitSdCard()
-{
-}
-
 void setup(void)
 {
   Serial.begin(9600);
@@ -153,21 +158,19 @@ void setup(void)
   gslc_Update(guiGui);
 
   initSdCard();
-  pixelOpen("rainbow.pix");
-  pixelLoadColumn(0);
-//  FastLED.setCorrection(TypicalLEDStrip);
-//  FastLED.setBrightness(25);
-//  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-}
 
-uint16_t col = 1;
+  File root = SD.open("/");
+  while (File entry = root.openNextFile()) {
+    Serial.println(entry.name());
+  }
+}
 
 void loop()
 {
   gslc_Update(guiGui);
 
-//  FastLED.show();
-  col = (col + 1) % pixelFile->columns;
+  static uint16_t col = 1;
+  col                 = (col + 1) % pixelFile->columns;
   pixelLoadColumn(col);
 }
 
@@ -177,7 +180,7 @@ void pixelOpen(const char *filename)
   Serial.print(F("Loading pixels "));
   Serial.println(filename);
 
-  pixelFile = new PixelFile;
+  pixelFile       = new PixelFile;
   pixelFile->file = SD.open(filename);
   if (!pixelFile->file) {
     panic(F("File not found"));
@@ -193,7 +196,7 @@ void pixelLoadColumn(uint16_t col)
 {
   const uint32_t pos = (col + 1) * 1024;
 
-  File& file = pixelFile->file;
+  File &file = pixelFile->file;
   if (file.position() != pos) {
     file.seek(pos);
   }
@@ -205,10 +208,10 @@ void pixelLoadColumn(uint16_t col)
 
 uint16_t pixelRead16()
 {
-  File& f = pixelFile->file;
+  File &   f = pixelFile->file;
   uint16_t result;
-  ((uint8_t*)&result)[0] = f.read(); // LSB
-  ((uint8_t*)&result)[1] = f.read(); // MSB
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
   return result;
 }
 
