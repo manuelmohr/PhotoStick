14263ecb89794363028ec582d8907e624de6120a
Manuel Mohr <manuelmohr@gmail.com>
2021-02-05 17:21:35 +0100

    Add some comments for BMP interface.

--- bmp.hpp
+++ bmp.hpp
@@ -17,7 +17,10 @@ struct BMPFile
   uint32_t imageOffset;
 };
 
+// Open given file as BMP file and save header information into struct
 void open(SdFat &sd, BMPFile &bmpFile, const char *filename);
+
+// Load pixel colors of row (counted starting from 0) into CRGB array
 void loadRow(BMPFile &bmpFile, uint32_t row, CRGB *leds);
 }
 
